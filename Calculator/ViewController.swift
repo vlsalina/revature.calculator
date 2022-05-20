1f09ceafd686cf239f318b52538bc17ea4989c23
Vincent Salinas <vince@Vincents-MacBook-Pro.local>
2022-05-20 07:59:49 -0700

    First commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vincent Salinas on 5/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
